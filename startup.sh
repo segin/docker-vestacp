448a9426f2cc881c7c41a20e301a8ab0d3841054
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 02:30:54 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,6 @@ then
 fi
 
 #starting Vesta
-chown root:root /var/run/sshd
 cd /etc/init.d/ && ./vesta start
 cd /etc/init.d/ && ./mysql start
 cd /etc/init.d/ && ./nginx start
@@ -17,3 +16,5 @@ cd /etc/init.d/ && ./apache2 start
 cd /etc/init.d/ && ./bind9 start
 cd /etc/init.d/ && ./dovecot start
 cd /etc/init.d/ && ./ssh start
+
+bash
